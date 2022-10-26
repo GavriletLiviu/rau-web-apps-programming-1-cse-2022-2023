9f0947df26490244773db8c1b08cecb97362b0c3
Liviu Cristian Gavrilet <liviu.gavrilet@gmail.com>
2022-10-26 19:18:43 +0300

    new stuffs

--- javascript-dom.js
+++ javascript-dom.js
@@ -27,3 +27,25 @@ for (const el of topNavButtons.children) {
 
 topNavButtons.appendChild(inviteButtonRemoved);
 topNavButtons.insertBefore(inviteButtonRemoved, accountButton[0])
+
+
+const classmates = document.getElementById("classmates");
+console.log(classmates);
+const courses = document.getElementById("courses");
+console.log(courses);
+const trade = document.getElementById("trade");
+console.log(trade);
+const profile = document.getElementById("profile");
+console.log(profile);
+classmates.style="color: #000080";
+courses.style="color:#00806b";
+profile.style="color:#807300";
+trade.style="color:#80006d"
+
+const rightArrow = document.getElementById("right-arrow");
+const course1 = document.getElementById("course1");
+const course2 = document.getElementById("course2");
+const course3 = document.getElementById("course3");
+if (rightArrow.click){
+    
+}
